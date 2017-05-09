880ff241fda8649e24e947b370e76fb77834162b
miaw0102 <miaw0102@gmail.com>
2017-05-09 10:04:28 +0800

    加入專案檔案。

---
+++ MVCHomeWork/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCHomeWork.Startup))]
+namespace MVCHomeWork
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
